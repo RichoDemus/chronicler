f9ef7e9e1890af3fbfb87b6baaf3412087f14c6b
RichoDemus <richodemus@gmail.com>
2017-03-19 19:29:15 +0100

    Some polish to the SSE test page

--- server/dropwizard/src/main/resources/webroot/sse.js
+++ server/dropwizard/src/main/resources/webroot/sse.js
@@ -1,49 +1,40 @@
-$(function ()
-{
+$(function () {
     console.log("window load");
-    setupServerSentEvents("asd");
+    setupServerSentEvents();
 });
 
-function setupServerSentEvents(id)
-{
+function setupServerSentEvents() {
     if (typeof(EventSource) === "undefined") {
         //browser does not support SSE
         alert("Your browser does not support Server Sent Events, get Chrome...");
         return;
     }
 
-    console.log("Creating EventSource");
     const eventSource = new EventSource("api/event-stream/");
-    eventSource.onmessage = function (event)
-    {
-        console.log("Got an event");
-        console.log(event);
-        document.getElementById('events').innerHTML += "New event with id " + event.data + "\n";
+    eventSource.onmessage = function (event) {
+
+        const textArea = document.getElementById('events');
+        textArea.innerHTML += "New event with id " + event.data + "\n";
+        textArea.scrollTop = textArea.scrollHeight;
+
     };
-    eventSource.onError = function ()
-    {
+    eventSource.onError = function () {
         console.log("onError")
     };
-    eventSource.onopen = function ()
-    {
+    eventSource.onopen = function () {
         console.log("Connection to server opened.");
     };
     //Tells the source not to reconnect if the server closes the connection
-    eventSource.addEventListener('close', function (event)
-    {
+    eventSource.addEventListener('close', function (event) {
         console.log("Got the close event");
         eventSource.close();
     }, false);
-    console.log("Shit should work now");
 }
 
-function createAndSendEvent()
-{
-    const uuid = guidGenerator();
-    const someData = Math.random().toString(36).substr(2, 5);
-    postData = {};
-    postData.id = uuid;
-    postData.data = someData;
+function createAndSendEvent() {
+    const postData = {};
+    postData.id = guidGenerator();
+    postData.data = Math.random().toString(36).substr(2, 5);
     $.ajax({
         url: "api/events",
         contentType: "application/json",
@@ -53,9 +44,8 @@ function createAndSendEvent()
 }
 // credits to http://stackoverflow.com/a/6860916/78679
 function guidGenerator() {
-    const S4 = function ()
-    {
+    const S4 = function () {
         return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
     };
-    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
 }
