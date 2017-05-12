3baa696678c900b114960847aadf5e1581fcdbf4
RichoDemus <richodemus@gmail.com>
2017-05-12 23:50:37 +0200

    Now properly uses DISK as the default persistence option

--- server/dropwizard/src/main/kotlin/com/richodemus/chronicler/server/dropwizard/inject/ChroniclerModule.kt
+++ server/dropwizard/src/main/kotlin/com/richodemus/chronicler/server/dropwizard/inject/ChroniclerModule.kt
@@ -7,11 +7,12 @@ import com.richodemus.chronicler.server.core.EventPersister
 
 class ChroniclerModule : AbstractModule() {
     override fun configure() {
-        val storage = System.getProperty("chronicler.storage", "DISK")
+        val storage = System.getProperty("chronicler.storage").orDefault("DISK")
         when (storage.toUpperCase()) {
             "DISK" -> bind(EventPersister::class.java).to(DiskEventPersister::class.java)
             "GCS" -> bind(EventPersister::class.java).to(GoogleCloudStoragePersistence::class.java)
             else -> throw IllegalArgumentException("Unsupported storage type \"$storage\"")
         }
     }
+    private fun String.orDefault(default: String) = if(this.orEmpty().isBlank()) default else this
 }
